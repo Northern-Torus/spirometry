146b3d096934cb7d2e3e41e8190b1034141db812
Duong Tran <ttduongtran@gmail.com>
2022-03-26 11:03:37 +0700

    dataset processing

--- README.md
+++ README.md
@@ -4,17 +4,25 @@
 
 ```python
 conda env create -f environment.yml
+
+```
+
+```sh
+conda activate torus-project 
 ```
 
 ## II.  Set up your dataset
 - Guide user how to download your data and set the data pipeline 
+
+Data processing script:
+```python
+python data.py
+```
 
 ## III. Training Process
 
 Training script:
 
 ```python
-
 python train.py --epochs ${epochs}
-
 ```
--- data.py
+++ data.py
@@ -1,9 +1,26 @@
+# import pyreadstat
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 class Dataset:
-    def __init__(self):
-        pass
+  def __init__(self, data_path):
+    self.data_path = data_path
+  
+  def clean_dataset(self, dataset):
+    # Fill na db values with 0
+    ds_clean = dataset.fillna(0);
 
-    def load_dataset(self):
-        pass
 
-    def build_dataset(self):
-        pass
+    return ds_clean;
+
+  def load_dataset(self):
+    dataset = pd.read_sas(self.data_path)  # ./data/SPX_E.XPT
+    return dataset;
+
+  def build_dataset(self):
+    print("Dataset built")
+    ds = self.clean_dataset(self.load_dataset())
+    print(ds);
+    
+    return ds
+    
